be3eb2bd6621b55f883f8b73bb800f7e7a1d097c
Dequingue <159256601+Dequingue@users.noreply.github.com>
2025-02-05 15:03:00 -0300

    Update Entrada2.java

--- atividade-JV/Entrada2.java
+++ atividade-JV/Entrada2.java
@@ -10,14 +10,21 @@ public class Entrada2 {
             System.out.println("Digite o segundo número: ");
             double n2=sc.nextDouble();
             
-            double soma =n1+n2;
+            System.out.println("Digite o terceiro número: ");
+            double n3=sc.nextDouble();
+
+            double soma =n1+n2+n3;
             double multi = n1*n2;
             double div = n1/n2;
-            
+            double media = soma/3;
+
             System.out.println(soma);
             System.out.println(multi);
             System.out.println(div);
+            System.out.println(media);
+            
         }
     }
     
 }
+
